15f55f709734f26a02a2c3aac7e8c152ade9249c
poojag021094 <poojag021094@gmail.com>
2018-06-26 22:42:54 +0530

    fundamentals

--- main.js
+++ main.js
@@ -32,3 +32,39 @@ if(!localStorage.getItem('name'))
   var storedName = localStorage.getItem('name');
   myHeading.textContent = 'Hello!' +' ' + storedName;
 }
+
+var machineButton = document.getElementById('myButton');
+var machinePara = document.getElementById('machine');
+
+machineButton.addEventListener('click',updateMachine);
+
+//Update machine status function
+function updateMachine(){
+  if(machineButton.textContent === 'Start the Machine'){
+    machineButton.textContent = 'Stop the Machine';
+    machinePara.textContent = 'Machine has started';
+  }else {
+    machineButton.textContent = 'Start the Machine';
+    machinePara.textContent = 'Machine has Stopped';
+  }
+}
+
+//Index2.html Script goes here
+/*
+var list = document.querySelector('.output ul');
+list.innerHTML = ' ';
+
+var greetings  = ['Happy Birthday!',
+                'Merry Christmas my love!',
+                'A happy Christmas to all the family!',
+                'you\'re all i want for my Christmas!',
+                'Get well soon'];
+for (var i = 0; i < greetings.length; i++) {
+  var input = greetings[i];
+  if(greetings[i].index('Christmas') !== -1){
+    var result = input;
+    var listitem = document.createElement('li');
+    listitem.textContent = result;
+    list.appendChild(listitem);
+  }
+}*/
